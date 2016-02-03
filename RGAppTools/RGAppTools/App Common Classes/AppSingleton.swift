0084fc32cdbb146f9777828201140a9b755fcd4b
Smartech <rain2540@foxmail.com>
2016-02-03 16:26:48 +0800

    Add explanations for AppSingleton

--- RGAppTools/RGAppTools/App Common Classes/AppSingleton.swift
+++ RGAppTools/RGAppTools/App Common Classes/AppSingleton.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
+/// Application
 public let kApplication = UIApplication.sharedApplication()
 
+/// AppDelegate
 internal let kAppDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
 
+/// Main Window
 public let kAppMainWindow = (UIApplication.sharedApplication().delegate as? AppDelegate)?.window
 
+/// Root View Controller
 public let kRootViewController = (UIApplication.sharedApplication().delegate as? AppDelegate)?.window?.rootViewController
 
+/// Main Screen
 public let kMainScreen = UIScreen.mainScreen()
