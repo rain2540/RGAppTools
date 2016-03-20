db622455ec2d607741054fd4bcfb4daeefb7b726
rain2540 <rain2540@foxmail.com>
2016-03-20 11:28:27 +0800

    Add comment to the extension of NSDictionary

--- RGAppTools/RGAppTools/Cocoa Types/NSDictionary+Extension.swift
+++ RGAppTools/RGAppTools/Cocoa Types/NSDictionary+Extension.swift
@@ -9,6 +9,14 @@
 import Foundation
 
 extension NSDictionary {
+    /**
+     获取 Main Bundle 中某个文件的路径
+     
+     - parameter name: 文件名
+     - parameter ext:  文件扩展名
+     
+     - returns: 文件路径
+     */
     convenience init?(mainBundlePathForResource name: String?, ofType ext: String?) {
         guard let path = NSBundle.mainBundle().pathForResource(name, ofType: ext) else {
             print("RGApptools: NSDictionary init with main bundle path for resource of type is nil")
