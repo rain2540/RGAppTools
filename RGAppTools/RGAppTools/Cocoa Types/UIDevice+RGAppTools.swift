699769c15d10d929f313aecd75a3650309c31d25
Smartech <rain2540@foxmail.com>
2016-05-18 09:10:07 +0800

    Add "UIDevice+RGAppTools.swift"

---
+++ RGAppTools/RGAppTools/Cocoa Types/UIDevice+RGAppTools.swift
@@ -0,0 +1,28 @@
+//
+//  UIDevice+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by RAIN on 16/2/2.
+//  Copyright © 2016年 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension UIDevice {
+    /**
+     打印当前设备信息
+     */
+    public static func rg_deviceInformations() {
+        NSLog("currentDevice: %@", UIDevice.currentDevice())
+        
+        let device = UIDevice()
+        NSLog("device.name: %@", device.name)
+        NSLog("device.model:%@", device.model)
+        NSLog("device.localizedModel: %@", device.localizedModel)
+        
+        NSLog("device.systemName: %@", device.systemName)
+        NSLog("device.systemVersion: %@", device.systemVersion)
+        
+        NSLog("device.identifierForVendor: %@", device.identifierForVendor!)
+    }
+}
