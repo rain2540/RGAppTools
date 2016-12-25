e9386e738549824250b949a7ee926dbfc42d35ef
rain <rain2540@foxmail.com>
2016-12-26 06:47:19 +0800

    feat (String) : add a method for compare

--- RGAppTools/RGAppTools/Basic Types/String+RGAppTools.swift
+++ RGAppTools/RGAppTools/Basic Types/String+RGAppTools.swift
@@ -23,6 +23,11 @@ func < (_ lh: String, _ rh: String) -> Bool {
     return lh.compare(rh, options: .numeric) == .orderedAscending
 }
 
+infix operator <=
+func <= (_ lh: String, _ rh: String) -> Bool {
+    return lh.compare(rh, options: .numeric) != .orderedDescending
+}
+
 //  MARK: Clear nil
 extension String {
     /**
