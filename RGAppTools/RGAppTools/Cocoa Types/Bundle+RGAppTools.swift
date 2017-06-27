3257f654b69c505bb56e58e604d37de42db2ec5e
Smartech <rain2540@foxmail.com>
2017-06-27 08:57:25 +0800

    feat (Bundle) : add a computed property

--- RGAppTools/RGAppTools/Cocoa Types/Bundle+RGAppTools.swift
+++ RGAppTools/RGAppTools/Cocoa Types/Bundle+RGAppTools.swift
@@ -12,4 +12,12 @@ extension RGAppTools where Base: Bundle {
     public static var mainBundlePath: String {
         return Bundle.main.bundlePath
     }
+
+    public static var appVersion: String {
+        let infoPath = Bundle.main.path(forResource: "Info", ofType: "plist")
+        let infoDic = NSDictionary(contentsOfFile: infoPath!)
+        let version = infoDic!["CFBundleShortVersionString"] as! String
+        let build = infoDic!["CFBundleVersion"] as! String
+        return version + " " + "(" + build + ")"
+    }
 }
