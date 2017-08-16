f0481439e0f4dff542abad89d2ba58fb6121caaa
Smartech <rain2540@foxmail.com>
2017-08-16 08:32:13 +0800

    feat (DispatchQueue) : add a method

--- RGAppTools/RGAppTools/Extensions/Cocoa/DispatchQueue+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Cocoa/DispatchQueue+RGAppTools.swift
@@ -9,8 +9,23 @@
 import Foundation
 
 extension RGAppTools where Base: DispatchQueue {
-    func after(_ delay: TimeInterval, execute closure: @escaping () -> Void) {
+    public func after(_ delay: TimeInterval, execute closure: @escaping () -> Void) {
         base.asyncAfter(deadline: .now() + delay, execute: closure)
+    }
+
+    public func once(token: String, task: () -> ()) {
+        objc_sync_enter(Base.self)
+
+        defer {
+            objc_sync_exit(Base.self)
+        }
+
+        if Base.onceToken.contains(token) {
+            return
+        }
+        Base.onceToken.append(token)
+
+        task()
     }
 }
 
