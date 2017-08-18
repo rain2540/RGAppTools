1b3258aa2413c08bfe3c9c26bca6481ab7ea3887
Smartech <rain2540@foxmail.com>
2017-08-18 10:19:27 +0800

    feat (UIConstant) : add variables

--- RGAppTools/RGAppTools/Extensions/UIKit/UIConstant.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UIConstant.swift
@@ -7,3 +7,35 @@
 //
 
 import UIKit
+
+/// Status Bar 默认高度
+public let StatusBarDefaultHeight: CGFloat = 20.0
+
+/// Navigation Bar 默认高度
+public let NavigationBarDefaultHeight: CGFloat = 44.0
+
+/// Top Layout 默认高度
+public var TopLayoutDefaultHeight: CGFloat {
+    return StatusBarDefaultHeight + NavigationBarDefaultHeight
+}
+
+/// Tab Bar 默认高度
+public let TabBarDefaultHeight: CGFloat = 49.0
+
+/// Tool Bar 默认高度
+public let ToolBarDefaultHeight: CGFloat = 44.0
+
+/// 视图默认间距
+public let ViewDefaultInterval: CGFloat = 8.0
+
+/// Label 默认高度
+public let LabelDefaultHeight: CGFloat = 21.0
+
+/// Button 默认高度
+public let ButtonDefaultHeight: CGFloat = 44.0
+
+/// TextField 默认高度
+public let TextFieldDefaultHeight: CGFloat = 30.0
+
+/// TableViewCell 默认高度
+public let TableViewCellDefaultHeight: CGFloat = 44.0
