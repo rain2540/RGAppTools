39560480d9e77c0be37105fc0095bae4629a37c7
Smartech <rain2540@foxmail.com>
2017-08-22 10:10:42 +0800

    feat (UIViewController) : add a method

--- RGAppTools/RGAppTools/Extensions/UIKit/UIViewController+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UIViewController+RGAppTools.swift
@@ -7,3 +7,20 @@
 //
 
 import UIKit
+
+extension RGAppTools where Base: UIViewController {
+    /// 获取位于最上层的视图
+    public var topViewController: UIViewController {
+        if base is UITabBarController {
+            let tabBarController = base as! UITabBarController
+            return tabBarController.selectedViewController!.rat.topViewController
+        } else if base is UINavigationController {
+            let navigationController = base as! UINavigationController
+            return navigationController.visibleViewController!.rat.topViewController
+        } else if let presentedViewController = base.presentedViewController {
+            return presentedViewController.rat.topViewController
+        } else {
+            return base
+        }
+    }
+}
