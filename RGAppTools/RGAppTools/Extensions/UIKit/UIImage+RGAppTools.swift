e3973ce10ec171e5d293a01fff4ae8e6368c3ad9
Smartech <rain2540@foxmail.com>
2017-08-22 09:08:54 +0800

    feat (UIImage) : add methods

--- RGAppTools/RGAppTools/Extensions/UIKit/UIImage+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UIImage+RGAppTools.swift
@@ -17,4 +17,25 @@ extension RGAppTools where Base: UIImage {
         }
         return jpegData
     }
+
+    /// Image 对应的 Base64 String
+    public var base64String: String? {
+        guard let data = self.data else { return nil }
+        return data.base64EncodedString()
+    }
+
+    /// 将图片转换到新的尺寸, 并返回新尺寸图片对象
+    ///
+    /// - Parameter newSize: 图片的新尺寸
+    /// - Returns: 新尺寸图片对象
+    public func trans(to newSize: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContext(newSize)
+        base.draw(in: CGRect(x: 0.0, y: 0.0, width: newSize.width, height: newSize.height))
+        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else {
+            UIGraphicsEndImageContext()
+            return nil
+        }
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 }
