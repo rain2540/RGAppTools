571a733e47f431a46ff1a7bac9b07840893c6a8b
Smartech <rain2540@foxmail.com>
2017-08-22 09:49:57 +0800

    feat (UINavigationBar) :  add methods

--- RGAppTools/RGAppTools/Extensions/UIKit/UINavigationBar+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UINavigationBar+RGAppTools.swift
@@ -7,3 +7,26 @@
 //
 
 import UIKit
+
+extension RGAppTools where Base: UINavigationBar {
+    /// 修改 Navigation Bar 的背景颜色
+    ///
+    /// - Parameter color: 提供给 Navigation Bar 的背景的 tint color
+    static func barTintColor(_ color: UIColor) {
+        UINavigationBar.appearance().barTintColor = color
+    }
+
+    /// 修改 Navigation item 的 tint color
+    ///
+    /// - Parameter color: 提供给 Navigation item 的 tint color
+    static func tintColor(_ color: UIColor) {
+        UINavigationBar.appearance().tintColor = color
+    }
+
+    /// 修改 Navigation Bar 的文字颜色
+    ///
+    /// - Parameter color: 提供给 Navigation Bar 上文字的颜色
+    static func titleTextColor(_ color: UIColor) {
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: color]
+    }
+}
