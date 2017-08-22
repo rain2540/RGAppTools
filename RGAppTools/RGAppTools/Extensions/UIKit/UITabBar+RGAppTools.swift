2a35a70683e58e9ea87f0c044623d27e5f4769bc
Smartech <rain2540@foxmail.com>
2017-08-22 09:55:19 +0800

    feat (UITabBar) : add methods

--- RGAppTools/RGAppTools/Extensions/UIKit/UITabBar+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UITabBar+RGAppTools.swift
@@ -7,3 +7,19 @@
 //
 
 import UIKit
+
+extension RGAppTools where Base: UITabBar {
+    /// 修改 Tab Bar 的背景颜色
+    ///
+    /// - Parameter color: 提供给 Tab Bar 的背景的 tint color
+    static func barTintColor(_ color: UIColor) {
+        UITabBar.appearance().barTintColor = color
+    }
+
+    /// 修改 Tab Bar item 的 tint color
+    ///
+    /// - Parameter color: 提供给 Tab Bar item 的 tint color
+    static func tintColor(_ color: UIColor) {
+        UITabBar.appearance().tintColor = color
+    }
+}
