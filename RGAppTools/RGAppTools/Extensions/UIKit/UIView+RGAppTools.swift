635a1c45767de811c14269485a7df21fcf72c7ba
Smartech <rain2540@foxmail.com>
2017-08-22 11:15:59 +0800

    feat (UIView) : make some update
    1.add methods
    2.add comments

--- RGAppTools/RGAppTools/Extensions/UIKit/UIView+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UIView+RGAppTools.swift
@@ -8,47 +8,85 @@
 
 import UIKit
 
-enum RGLineDirection {
+public enum RGLineDirection {
     case horizontal
     case vertical
 }
 
 extension RGAppTools where Base: UIView {
+    // MARK: Draw
+    /// 在 view 的指定位置加指定长宽、颜色的线
+    ///
+    /// - Parameters:
+    ///   - startPoint: 起始点坐标
+    ///   - length: 长度
+    ///   - width: 宽度
+    ///   - color: 颜色
+    ///   - direction: 方向
+    /// - Returns: 按参数条件添加的线
+    public func drawLine(
+        from startPoint: CGPoint,
+        length: CGFloat,
+        width: CGFloat,
+        color: UIColor,
+        direction: RGLineDirection = .horizontal) -> UIView
+    {
+        let line = UIView()
+        switch direction {
+        case .horizontal:
+            line.frame = CGRect(x: startPoint.x, y: startPoint.y, width: length, height: width)
 
+        case .vertical:
+            line.frame = CGRect(x: startPoint.x, y: startPoint.y, width: width, height: length)
+        }
+        line.backgroundColor = color
+        base.addSubview(line)
+        return line
+    }
+
+    /// 为视图添加边线
+    ///
+    /// - Parameters:
+    ///   - width: 边线宽度
+    ///   - cornerRadius: 边线圆角半径, 若无圆角则该值为0
+    ///   - color: 边线颜色
+    public func addBorder(width: CGFloat, cornerRadius: CGFloat, color: UIColor) {
+        base.layer.masksToBounds = true
+        base.layer.borderWidth = width
+        base.layer.cornerRadius = cornerRadius
+        base.layer.borderColor = color.cgColor
+    }
+
+    // MARK: Interaction
+    /// 设置视图交互状态
+    ///
+    /// - Parameter status: 表示交互状态的布尔值
+    public func interaction(status: Bool) {
+        base.isUserInteractionEnabled = status
+        base.isMultipleTouchEnabled = status
+    }
+
+    // MARK: Rect
+    /// 视图尺寸
     var size: CGSize { return base.bounds.size }
 
+    /// 视图横坐标最小值
     var minX: CGFloat { return base.frame.minX }
+    /// 视图中心横坐标值
     var midX: CGFloat { return base.frame.midX }
+    /// 视图横坐标最大值
     var maxX: CGFloat { return base.frame.maxX }
 
+    /// 视图纵坐标最小值
     var minY: CGFloat { return base.frame.minY }
+    /// 视图中心纵坐标值
     var midY: CGFloat { return base.frame.midY }
+    /// 视图纵坐标最大值
     var maxY: CGFloat { return base.frame.maxY }
 }
 
 extension UIView {
-    var rat_width: CGFloat {
-        get {
-            return self.frame.size.width
-        }
-        set {
-            var frame = self.frame
-            frame.size.width = newValue
-            self.frame = frame
-        }
-    }
-
-    var rat_height: CGFloat {
-        get {
-            return self.frame.size.height
-        }
-        set {
-            var frame = self.frame
-            frame.size.height = newValue
-            self.frame = frame
-        }
-    }
-
+    /// 视图左上角横坐标值
     var rat_x: CGFloat {
         get {
             return self.frame.origin.x
@@ -60,6 +98,7 @@ extension UIView {
         }
     }
 
+    /// 视图左上角纵坐标值
     var rat_y: CGFloat {
         get {
             return self.frame.origin.y
@@ -67,6 +106,30 @@ extension UIView {
         set {
             var frame = self.frame
             frame.origin.y = newValue
+            self.frame = frame
+        }
+    }
+
+    /// 视图宽度
+    var rat_width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set {
+            var frame = self.frame
+            frame.size.width = newValue
+            self.frame = frame
+        }
+    }
+
+    /// 视图高度
+    var rat_height: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set {
+            var frame = self.frame
+            frame.size.height = newValue
             self.frame = frame
         }
     }
