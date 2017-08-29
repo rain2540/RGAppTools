42e0c68fe7e054365aa9d8a39b493f35010a6da8
Smartech <rain2540@foxmail.com>
2017-08-29 11:06:32 +0800

    feat (UIImage) : update the method create new rectangle image

--- RGAppTools/RGAppTools/Extensions/UIKit/UIImage+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UIImage+RGAppTools.swift
@@ -24,18 +24,57 @@ extension RGAppTools where Base: UIImage {
         return data.base64EncodedString()
     }
 
-    /// 将图片转换到新的尺寸, 并返回新尺寸图片对象
-    ///
-    /// - Parameter newSize: 图片的新尺寸
-    /// - Returns: 新尺寸图片对象
-    public func trans(to newSize: CGSize) -> UIImage? {
-        UIGraphicsBeginImageContext(newSize)
-        base.draw(in: CGRect(x: 0.0, y: 0.0, width: newSize.width, height: newSize.height))
-        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else {
-            UIGraphicsEndImageContext()
-            return nil
+     /// 创建矩形图像
+     ///
+     /// - Parameters:
+     ///   - size: 尺寸
+     ///   - backColor: 背景色(默认`white`)
+     ///   - lineColor: 边线颜色(默认`lightGray`)
+     ///   - lineWidth: 变现宽度(默认`1.0`)
+     /// - Returns: 新创建的图像
+     public func createRectImage(size: CGSize?,
+                                backColor: UIColor = .white,
+                                lineColor: UIColor = .lightGray,
+                                lineWidth: CGFloat = 1.0) -> UIImage?
+    {
+        var size = size
+        if size == nil {
+            size = base.size
         }
+
+        // 依据新图片的大小创建绘制区域
+        let rect = CGRect(origin: .zero, size: size!)
+
+        // 1.图像的上下文 - 内存中开辟一个地址, 跟屏幕无关
+        /**
+         * 1> size:   绘图的尺寸
+         * 2> opaque: 不透明:false(透明) / true(不透明)
+         * 3> scale:  屏幕分辨率,默认情况下生成的图像使用'1.0'的分辨率,图像质量不好
+         *            可以指定'0',会选择当前设备的屏幕分辨率
+         */
+        UIGraphicsBeginImageContextWithOptions(rect.size, true, 0.0)
+
+        // 2.背景填充(在裁切之前做填充)
+        backColor.setFill()
+        UIRectFill(rect)
+
+        // 3.绘图'drawInRect'就是在指定区域内重绘图片
+        base.draw(in: rect)
+
+        // 4.绘制边线
+        // 按照新尺寸的图片边界实例化一个路径
+        let path = UIBezierPath(rect: rect)
+        lineColor.setStroke()
+        path.lineWidth = lineWidth
+        path.stroke()
+
+        // 4.取得结果
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+
+        // 5.关闭上下文
         UIGraphicsEndImageContext()
-        return newImage
+        
+        // 6.返回结果
+        return result
     }
 }
