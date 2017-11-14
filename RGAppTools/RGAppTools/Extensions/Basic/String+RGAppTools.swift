427b39cad9f19232ec5a78a3c9557ada10f4de28
Rain <rain2540@foxmail.com>
2017-11-14 15:55:06 +0800

    refactor (String) : modify a method
    delete string.characters

--- RGAppTools/RGAppTools/Extensions/Basic/String+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Basic/String+RGAppTools.swift
@@ -82,7 +82,7 @@ extension String {
     ///   - newElement: 插入的字符
     ///   - i: 指定位置索引
     public mutating func rat_insert(_ newElement: Character, at i: Int) {
-        guard (i <= characters.count) && (i >= 0) else {
+        guard (i <= self.count) && (i >= 0) else {
             print("\(#function): index out of range")
             return
         }
