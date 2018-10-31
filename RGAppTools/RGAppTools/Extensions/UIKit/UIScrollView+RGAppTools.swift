98dff1bf6c1d1b5b0a052fbe75d26f38ebc08e7b
Smartech <rain2540@foxmail.com>
2018-10-31 09:23:19 +0800

    feat (UIScrollView) : add a method

--- RGAppTools/RGAppTools/Extensions/UIKit/UIScrollView+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UIScrollView+RGAppTools.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 extension RGAppTools where Base: UIScrollView {
+    public func registerNibCell(with cellClass: AnyClass) {
+        if let nibName = anyClassToString(cellClass) {
+            let nib = UINib(nibName: nibName, bundle: nil)
+
+            if let tableView = base as? UITableView {
+                tableView.register(nib, forCellReuseIdentifier: nibName)
+            } else if let collectionView = base as? UICollectionView {
+                collectionView.register(nib, forCellWithReuseIdentifier: nibName)
+            }
+        }
+    }
+    
     private func anyClassToString(_ className: AnyClass) -> String? {
         let desc = className.description()
         guard desc.contains(".") else {
