acb5d1a1583ec3c998f7bbd68be7e6ddb3bd38af
Smartech <rain2540@foxmail.com>
2019-01-07 16:48:57 +0800

    feat (Array) : add a new method special elements

--- RGAppTools/RGAppTools/Extensions/Basic/Array+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Basic/Array+RGAppTools.swift
@@ -9,6 +9,33 @@
 import Foundation
 
 extension Array {
+
+    subscript(first: Int, second: Int, others: Int...) -> ArraySlice<Element> {
+        get {
+            var indexes: [Int] = []
+            indexes.append(contentsOf: [first, second])
+            indexes.append(contentsOf: others)
+
+            var result = ArraySlice<Element>()
+            for i in indexes {
+                assert(i < self.count, "Index is out of range")
+                result.append(self[i])
+            }
+            return result
+        }
+
+        set {
+            var indexes: [Int] = []
+            indexes.append(contentsOf: [first, second])
+            indexes.append(contentsOf: others)
+
+            for (index, i) in indexes.enumerated() {
+                assert(i < self.count, "Index is out of range")
+                self[i] = newValue[index]
+            }
+        }
+    }
+
     subscript(input: [Int]) -> ArraySlice<Element> {
         get {
             var result = ArraySlice<Element>()
