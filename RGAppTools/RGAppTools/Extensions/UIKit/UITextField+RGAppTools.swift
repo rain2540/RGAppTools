6c44f757d79490e47941f56780d299980a2febb8
Smartech <rain2540@foxmail.com>
2019-02-22 09:02:44 +0800

    feat (UITextField) : add an extension of Text Field

---
+++ RGAppTools/RGAppTools/Extensions/UIKit/UITextField+RGAppTools.swift
@@ -0,0 +1,13 @@
+//
+//  UITextField+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2019/2/22.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension RGAppTools where Base: UITextField {
+
+}
