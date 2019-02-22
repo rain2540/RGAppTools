07dcc94b49e0c8d93f0f663c82a6718735a265b0
Smartech <rain2540@foxmail.com>
2019-02-22 09:17:01 +0800

    feat (UITextField) : add a method

--- RGAppTools/RGAppTools/Extensions/UIKit/UITextField+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UITextField+RGAppTools.swift
@@ -10,4 +10,17 @@ import UIKit
 
 extension RGAppTools where Base: UITextField {
 
+    public func addRightImage(named name: String,
+                              frame: CGRect = CGRect(x: 0.0, y: 0.0, width: 20.0, height: 20.0),
+                              contentMode: UIView.ContentMode = .center,
+                              showMode: UITextField.ViewMode = .always)
+    {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: name)
+        imageView.frame = frame
+        imageView.contentMode = contentMode
+        base.rightViewMode = showMode
+        base.rightView = imageView
+    }
+
 }
