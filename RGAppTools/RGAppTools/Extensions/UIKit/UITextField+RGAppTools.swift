7b1095aaec56536dc2078439b70c07ccbdd24df8
Smartech <rain2540@foxmail.com>
2019-02-25 08:24:29 +0800

    docs (UITextField) : add a comment

--- RGAppTools/RGAppTools/Extensions/UIKit/UITextField+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UITextField+RGAppTools.swift
@@ -10,6 +10,13 @@ import UIKit
 
 extension RGAppTools where Base: UITextField {
     
+    /// 为 TextField 添加左侧图片
+    ///
+    /// - Parameters:
+    ///   - name: 图片名
+    ///   - frame: 图片的 frame
+    ///   - contentMode: 图片的 content mode
+    ///   - showMode: 图片的显示模式
     public func addLeftImage(named name: String,
                              frame: CGRect = CGRect(x: 0.0, y: 0.0, width: 20.0, height: 20.0),
                              contentMode: UIView.ContentMode = .center,
@@ -34,7 +41,8 @@ extension RGAppTools where Base: UITextField {
         base.rightView = imageView
     }
     
-    
+
+    // MARK: - Private
     private func createImageView(imageNamed name: String,
                                  frame: CGRect,
                                  contentMode: UIView.ContentMode) -> UIImageView
