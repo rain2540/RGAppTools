d2d369f1d5712cf8da6f89a3f829a1344875ba8a
Rain <rain2540@foxmail.com>
2019-03-31 10:27:56 +0800

    refactor (String) : update implementation

--- RGAppTools/RGAppTools/Extensions/Basic/String+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Basic/String+RGAppTools.swift
@@ -63,7 +63,7 @@ extension String {
     /// - Parameter original: 待检验的索引值
     /// - Returns: 经过检验的有效索引值或 nil
     fileprivate func rat_validStartIndex(original: Int) -> String.Index? {
-        guard original <= endIndex.encodedOffset else { return nil }
+        guard original < count else { return nil }
         return rat_validIndex(original: original)
     }
 
