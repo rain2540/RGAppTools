e0001bba20983fdf507dfa2b3eae25b87574dc7b
Smartech <rain2540@foxmail.com>
2019-04-01 08:13:29 +0800

    docs (UIConstant) : add a mark

--- RGAppTools/RGAppTools/App Commons/UIConstant.swift
+++ RGAppTools/RGAppTools/App Commons/UIConstant.swift
@@ -48,6 +48,7 @@ public let TableViewCellDefaultHeight: CGFloat = 44.0
 public let HomeIndicatorAreaHeight: CGFloat = 34.0
 
 
+// MARK: - Deprecated
 @available(*, deprecated, message: "Extensions directly on is_iPhoneX in UIConstant is deprecated. Use `UIDevice.rat.isIPhoneXSeries` instead.", renamed: "UIDevice.rat.isIPhoneXSeries")
 public var is_iPhoneX: Bool {
     return (UIScreen.main.bounds.height == 812.0 && UIScreen.main.bounds.width == 375.0)
