61a20f4c1ce98dd06888440c7a1d78727d09b355
Rain <rain2540@foxmail.com>
2019-04-20 10:02:23 +0800

    docs (Bundle) : update comment

--- RGAppTools/RGAppTools/Extensions/Cocoa/Bundle+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Cocoa/Bundle+RGAppTools.swift
@@ -10,8 +10,8 @@ import Foundation
 
 /// 应用版本号 Build 字段格式
 ///
-/// - normal: 括号
-/// - dot: 点
+/// - normal: 括号 例如: 1.0.0 (29)
+/// - dot: 点  例如: 1.0.0.29
 public enum BuildVersionFormat {
     case normal
     case dot
