c5a3ce53e3a021a19507898cd2388baa8a7188cf
Rain <rain2540@foxmail.com>
2019-06-15 08:08:01 +0800

    feat (Optional) : add a method

--- RGAppTools/RGAppTools/Extensions/Basic/Optional+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Basic/Optional+RGAppTools.swift
@@ -62,6 +62,10 @@ extension Optional {
         guard let unwrapped = self else { throw exception }
         return unwrapped
     }
+    
+    public func map<T>(_ fn: (Wrapped) throws -> T, default: T) rethrows -> T {
+        return try map(fn) ?? `default`
+    }
 }
 
 
