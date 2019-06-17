5af50dd883f7f65eef72773bd6651c12e66f8010
Smartech <rain2540@foxmail.com>
2019-06-17 08:17:04 +0800

    style (Optional) : update style

--- RGAppTools/RGAppTools/Extensions/Basic/Optional+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Basic/Optional+RGAppTools.swift
@@ -62,11 +62,14 @@ extension Optional {
         guard let unwrapped = self else { throw exception }
         return unwrapped
     }
-    
+}
+
+
+extension Optional {
     public func rat_map<T>(_ fn: (Wrapped) throws -> T, default: T) rethrows -> T {
         return try map(fn) ?? `default`
     }
-    
+
     public func rat_map<T>(_ fn: (Wrapped) throws -> T, else: () throws -> T) rethrows -> T {
         return try map(fn) ?? `else`()
     }
