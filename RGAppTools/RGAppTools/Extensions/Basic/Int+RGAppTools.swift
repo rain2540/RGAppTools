61bb3d3b3485cf8fe6dfd6cf3add1a493372f703
Smartech <rain2540@foxmail.com>
2019-06-18 08:27:49 +0800

    refactor (Int) : modify a method name

--- RGAppTools/RGAppTools/Extensions/Basic/Int+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/Basic/Int+RGAppTools.swift
@@ -42,7 +42,7 @@ public struct IntExtension {
 }
 
 extension IntExtension {
-    public static func randomIntNumber(lower: Int = 0, upper: Int = Int(UInt32.max)) -> Int {
+    public static func randomNumber(lower: Int = 0, upper: Int = Int(UInt32.max)) -> Int {
         return lower + Int(arc4random_uniform(UInt32(upper - lower)))
     }
 }
