d46995770233d1cdf2f153760bc74473c4852883
Smartech <rain2540@foxmail.com>
2019-07-30 11:26:41 +0800

    docs (MainScreen) : add comment

--- RGAppTools/RGAppTools/RGBuild/MainScreen.swift
+++ RGAppTools/RGAppTools/RGBuild/MainScreen.swift
@@ -10,6 +10,7 @@ import UIKit
 
 public struct MainScreen {
 
+    /// scale of main screen
     public static var scale: CGFloat { return self.mainScreen.scale }
     /// width of main screen
     public static var width: CGFloat { return self.size.width }
