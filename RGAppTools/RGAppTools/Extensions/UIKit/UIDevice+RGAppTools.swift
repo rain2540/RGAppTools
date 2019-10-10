cc655a6d7731c6a311ab7989d7a1e83399c4ab70
Smartech <rain2540@foxmail.com>
2019-10-10 10:20:36 +0800

    feat (UIDevice) : add a return value of device type

--- RGAppTools/RGAppTools/Extensions/UIKit/UIDevice+RGAppTools.swift
+++ RGAppTools/RGAppTools/Extensions/UIKit/UIDevice+RGAppTools.swift
@@ -241,6 +241,10 @@ extension RGAppTools where Base: UIDevice {
             print("iPhone 11")
             return .iPhone_11
 
+        case "iPhone12,3":
+            print("iPhone 11 Pro")
+            return .iPhone_11_Pro
+
         case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":
             print("iPad 2")
             return .iPad_2
