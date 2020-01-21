a033653b690f4bdd23605768a80281635c85c448
Smartech <rain2540@foxmail.com>
2020-01-21 13:33:39 +0800

    test (String) : add StringExtensionsTests

---
+++ RGAppToolsTests/StringExtensionsTests.swift
@@ -0,0 +1,19 @@
+//
+//  StringExtensionsTests.swift
+//  RGAppToolsTests
+//
+//  Created by RAIN on 2020/1/21.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import XCTest
+@testable import RGAppTools
+
+class StringExtensionTests: XCTestCase {
+
+    func testStringTrans() {
+        let s = "5"
+        XCTAssertEqual(s.rat.intValue, 5)
+    }
+
+}
