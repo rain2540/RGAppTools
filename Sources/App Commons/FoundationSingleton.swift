14445f09eafc9ce04359f4521d6738fc9a4a0576
Smartech <rain2540@foxmail.com>
2020-01-21 10:52:56 +0800

    feat (FoundationSingleton) : add FoundationSingleton

---
+++ Sources/App Commons/FoundationSingleton.swift
@@ -0,0 +1,21 @@
+//
+//  FoundationSingleton.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2020/1/21.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import Foundation
+
+/// Standard UserDefaults
+public let StandardUserDefaults = UserDefaults.standard
+
+/// Default Notification Center
+public let DefaultNotificationCenter = NotificationCenter.default
+
+/// Default File Manager
+public let DefaultFileManager = FileManager.default
+
+/// Main Bundle
+public let MainBundle = Bundle.main
