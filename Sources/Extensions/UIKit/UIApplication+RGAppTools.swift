1e5154b5376fe0ed932d77aae9f33d45e811a6a2
Smartech <rain2540@foxmail.com>
2020-01-21 16:06:09 +0800

    feat (UIApplication) : extension of UIApplication

---
+++ Sources/Extensions/UIKit/UIApplication+RGAppTools.swift
@@ -0,0 +1,15 @@
+//
+//  UIApplication+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2020/1/21.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension RGAppTools where Base: UIApplication {
+
+
+
+}
