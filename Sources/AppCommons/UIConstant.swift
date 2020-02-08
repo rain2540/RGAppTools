eeee2bbeb39a79d21dfaa84a5d3c107ece283734
Rain2540 <rain2540@foxmail.com>
2020-02-09 00:21:25 +0800

    refactor (UIConstant) : deprecated a constant

--- Sources/AppCommons/UIConstant.swift
+++ Sources/AppCommons/UIConstant.swift
@@ -12,9 +12,6 @@ import UIKit
 /// Tool Bar 默认高度
 public let ToolBarDefaultHeight: CGFloat = 44.0
 
-/// 视图默认间距
-public let ViewDefaultInterval: CGFloat = 8.0
-
 /// Label 默认高度
 public let LabelDefaultHeight: CGFloat = 21.0
 
@@ -36,6 +33,10 @@ public let HomeIndicatorAreaHeight: CGFloat = 34.0
 public var is_iPhoneX: Bool {
     return (UIScreen.main.bounds.height == 812.0 && UIScreen.main.bounds.width == 375.0)
 }
+
+/// 视图默认间距
+@available(*, deprecated, message: "Constant 'ViewDefaultInterval' is deprecated. Use`UIView.rat.defaultInterval` instead.", renamed: "UIView.rat.defaultInterval")
+public let ViewDefaultInterval: CGFloat = 8.0
 
 /// Status Bar 的 Frame
 @available(*, deprecated, message: "Constant 'StatusBarFrame' is deprecated. Use`UIApplication.rat.statusBarFrame` instead.", renamed: "UIApplication.rat.statusBarFrame")
