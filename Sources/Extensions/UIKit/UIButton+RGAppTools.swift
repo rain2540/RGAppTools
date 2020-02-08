b98eba0fae943b9ed08ed52ee8dd6c3d0ee73bb7
Rain2540 <rain2540@foxmail.com>
2020-02-09 00:43:30 +0800

    feat (project) : add extension for UIButton

---
+++ Sources/Extensions/UIKit/UIButton+RGAppTools.swift
@@ -0,0 +1,13 @@
+//
+//  UIButton+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by Rain on 2020/2/9.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension RGAppTools where Base: UIButton {
+    
+}
