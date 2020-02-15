5a4793c4c6cb37b3446dfb395af66d694b9d7c7d
Rain2540 <rain2540@foxmail.com>
2020-02-16 00:35:53 +0800

    feat (project) : add extension for UILabel

---
+++ Sources/Extensions/UIKit/UILabel+RGAppTools.swift
@@ -0,0 +1,15 @@
+//
+//  UILabel+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by Rain on 2020/2/16.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension RGAppTools where Base: UILabel {
+    
+    
+    
+}
