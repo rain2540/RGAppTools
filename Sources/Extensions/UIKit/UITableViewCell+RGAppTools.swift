029ade0766170abde6e5560073adffeb33d5cdd1
Rain2540 <rain2540@foxmail.com>
2020-02-17 09:37:41 +0800

    feat (project) : add extension for UITableViewCell

---
+++ Sources/Extensions/UIKit/UITableViewCell+RGAppTools.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by Rain on 2020/2/17.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension RGAppTools where Base: UITableViewCell {
+    
+    
+    
+}
