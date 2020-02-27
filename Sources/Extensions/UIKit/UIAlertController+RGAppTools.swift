af2236519e2ac5476fca38bb1d18f343bb385d1b
Rain2540 <rain2540@foxmail.com>
2020-02-27 10:39:03 +0800

    feat (UIAlertController) : private method for create alert view

--- Sources/Extensions/UIKit/UIAlertController+RGAppTools.swift
+++ Sources/Extensions/UIKit/UIAlertController+RGAppTools.swift
@@ -58,4 +58,17 @@ extension RGAppTools where Base: UIAlertController {
         }
         viewController.present(actionSheet, animated: true, completion: nil)
     }
+    
+    
+    // MARK: - Private
+    private static func alert(title: String,
+                             message: String,
+                             actions: [UIAlertAction]) -> UIAlertController
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for action in actions {
+            alert.addAction(action)
+        }
+        return alert
+    }
 }
