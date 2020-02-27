38055068dafd8f13f502afdab97ef62a2f9cb2b0
Rain2540 <rain2540@foxmail.com>
2020-02-27 10:10:58 +0800

    feat (UIAlertController) : create an alert

--- Sources/Extensions/UIKit/UIAlertController+RGAppTools.swift
+++ Sources/Extensions/UIKit/UIAlertController+RGAppTools.swift
@@ -28,6 +28,17 @@ extension RGAppTools where Base: UIAlertController {
         }
         viewController.present(alert, animated: true, completion: nil)
     }
+    
+    public static func alert(title: String,
+                             message: String,
+                             actions: UIAlertAction...) -> UIAlertController
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for action in actions {
+            alert.addAction(action)
+        }
+        return alert
+    }
 
     /// 在视图中添加 ActionSheet
     ///
