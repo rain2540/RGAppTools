718caa856ab77b32fc47e4eaf51158a98ea527d5
Smartech <rain2540@foxmail.com>
2020-04-21 10:22:21 +0800

    feat (RGPropertyWrapper) : add property wrapper 'UserDefault'

--- Sources/RGBuild/RGPropertyWrapper.swift
+++ Sources/RGBuild/RGPropertyWrapper.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+
+public struct RGPropertyWrapper {
+
+    @propertyWrapper
+    public struct UserDefault<T> {
+        let key: String
+        let defaultValue: T
+
+        public var wrappedValue: T {
+            get {
+                return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+            }
+            set {
+                UserDefaults.standard.set(newValue, forKey: key)
+            }
+        }
+
+        public init(key: String, defaultValue: T) {
+            self.key = key
+            self.defaultValue = defaultValue
+        }
+    }
+
+}
