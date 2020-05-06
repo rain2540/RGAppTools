a79c4d70e479c313c41f80e31681a38fc511f741
Smartech <rain2540@foxmail.com>
2020-05-06 09:04:56 +0800

    docs (Optional) : update comment

--- Sources/Extensions/Basic/Optional+RGAppTools.swift
+++ Sources/Extensions/Basic/Optional+RGAppTools.swift
@@ -116,10 +116,11 @@ extension Optional {
     }
 
     /// 可选值变换返回，如果可选值为空，则调用 `else` 闭包
-    ///
     /// - Parameters:
     ///   - fn: 映射值的闭包
     ///   - else: 可选值为空时，调用的闭包
+    /// - Throws: 抛出的异常信息
+    /// - Returns: 变换后的结果
     public func rat_map<T>(_ fn: (Wrapped) throws -> T, else: () throws -> T) rethrows -> T {
         return try map(fn) ?? `else`()
     }
