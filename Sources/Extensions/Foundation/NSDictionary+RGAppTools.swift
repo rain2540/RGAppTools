06e4d00299399c0d7bd19c18227affc2b3fd7494
Smartech <rain2540@foxmail.com>
2020-09-01 13:18:17 +0800

    docs (NSDictionary) : update comments

--- Sources/Extensions/Foundation/NSDictionary+RGAppTools.swift
+++ Sources/Extensions/Foundation/NSDictionary+RGAppTools.swift
@@ -9,25 +9,25 @@
 import Foundation
 
 extension RGAppTools where Base: NSDictionary {
+
     /// 检验 NSDictionary 中是否存在某个 key
-    ///
     /// - Parameter key: 待检验的 key
     /// - Returns: 检验结果的布尔值
     public func isHave(key: String) -> Bool {
        return base.value(forKey: key) != nil
     }
+
 }
 
-//  MARK: Main Bundle
+
+// MARK: - Main Bundle
+
 extension NSDictionary {
-    /**
-     获取 Main Bundle 中某个文件的内容, 创建为 NSDictionary
-     
-     - parameter name: 文件名
-     - parameter ext:  文件扩展名
-     
-     - returns: 文件路径
-     */
+
+    /// 获取 Main Bundle 中某个文件的内容, 创建为 NSDictionary
+    /// - Parameters:
+    ///   - name: 文件名
+    ///   - ext: 文件扩展名
     public convenience init?(mainBundlePathForResource name: String?, ofType ext: String?) {
         guard let path = Bundle.main.path(forResource: name, ofType: ext) else {
             print("RGApptools: NSDictionary init with main bundle path for resource of type is nil")
@@ -35,17 +35,17 @@ extension NSDictionary {
         }
         self.init(contentsOfFile: path)
     }
+
 }
 
-//  MARK: Check key exist or not
+
+// MARK: - Check key exist or not
+
 extension NSDictionary {
-    /**
-     检验 NSDictionary 中是否存在某个 key
-     
-     - parameter key: 待检验的 key
-     
-     - returns: 检验结果的布尔值
-     */
+
+    /// 检验 NSDictionary 中是否存在某个 key
+    /// - Parameter key: 待检验的 key
+    /// - Returns: 检验结果的布尔值
     @available(*, deprecated, message: "Extensions directly on NSDictionary are deprecated. Use `NSDictionary.rat.isHave(key:)` instead.", renamed: "rat.isHave(key:)")
     func rat_isHaveKey(_ key: String) -> Bool {
         let keys = self.allKeys as! [String]
@@ -56,4 +56,5 @@ extension NSDictionary {
         }
         return false
     }
+
 }
