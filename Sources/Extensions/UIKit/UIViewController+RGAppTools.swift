04e0f7483393b02b4d6c806736cde5542fe059b5
Smartech <rain2540@foxmail.com>
2020-09-09 14:06:36 +0800

    refactor (UIViewController) : update topViewController

--- Sources/Extensions/UIKit/UIViewController+RGAppTools.swift
+++ Sources/Extensions/UIKit/UIViewController+RGAppTools.swift
@@ -11,18 +11,8 @@ import UIKit
 extension RGAppTools where Base: UIViewController {
 
     /// 获取位于最上层的视图
-    public var topViewController: UIViewController {
-        if base is UITabBarController {
-            let tabBarController = base as! UITabBarController
-            return tabBarController.selectedViewController!.rat.topViewController
-        } else if base is UINavigationController {
-            let navigationController = base as! UINavigationController
-            return navigationController.visibleViewController!.rat.topViewController
-        } else if let presentedViewController = base.presentedViewController {
-            return presentedViewController.rat.topViewController
-        } else {
-            return base
-        }
+    public var topViewController: UIViewController? {
+        UIViewController.rat.topViewController(of: base)
     }
 
     public static var topViewControllerOfKeyWindowRoot: UIViewController? {
