6572747f1611804fe6d36117eda59c104575c0ec
Smartech <rain2540@foxmail.com>
2020-09-09 14:11:13 +0800

    docs (UIViewController) : add comments

--- Sources/Extensions/UIKit/UIViewController+RGAppTools.swift
+++ Sources/Extensions/UIKit/UIViewController+RGAppTools.swift
@@ -15,27 +15,35 @@ extension RGAppTools where Base: UIViewController {
         UIViewController.rat.topViewController(of: base)
     }
 
+    /// 获取当前屏幕显示的 view controller
     public static var topViewControllerOfKeyWindowRoot: UIViewController? {
         guard let rootVC = UIApplication.shared.keyWindow?.rootViewController else { return nil }
         let topVC = topViewController(of: rootVC)
         return topVC
     }
 
+    /// 获取 root view controller 的最上层视图
+    /// - Parameter rootViewController: 需要获取 top view controller 的根视图
+    /// - Returns: 获取到的 最上层视图
     public static func topViewController(of rootViewController: UIViewController) -> UIViewController? {
         var rootVC = rootViewController
         var topVC: UIViewController? = nil
 
         if rootVC.presentedViewController != nil {
+            // 根视图是被 present 出来的
             rootVC = rootVC.presentedViewController!
         }
 
         if rootVC is UITabBarController {
+            // 根视图为 UITabBarController
             let tabBarController = rootVC as! UITabBarController
             topVC = topViewController(of: tabBarController.selectedViewController!)
         } else if rootVC is UINavigationController {
+            // 根视图为 UINavigationController
             let navigationController = rootVC as! UINavigationController
             topVC = topViewController(of: navigationController.visibleViewController!)
         } else {
+            // 根视图非导航类
             topVC = rootVC
         }
 
