eb7113ecb107fd2fa5730c8eea2aa014b156ae48
Smartech <rain2540@foxmail.com>
2020-11-30 14:36:32 +0800

    chore (project) : add extension of Color

---
+++ Sources/Extensions/SwiftUI/Color+RGAppTools.swift
@@ -0,0 +1,16 @@
+//
+//  Color+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2020/11/30.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension Color {
+
+
+
+}
