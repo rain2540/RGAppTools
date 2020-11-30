ff9e00c2ab6022533f8da90f46b2e65b495ec247
Smartech <rain2540@foxmail.com>
2020-11-30 14:42:47 +0800

    feat (Color) : add initializer by RGBColorSpace, hex value, opacity value

--- Sources/Extensions/SwiftUI/Color+RGAppTools.swift
+++ Sources/Extensions/SwiftUI/Color+RGAppTools.swift
@@ -11,6 +11,19 @@ import SwiftUI
 @available(iOS 13.0, *)
 extension Color {
 
-
+    init(_ colorSpace: Color.RGBColorSpace = .sRGB, hex: Int, opacity: Double = 1.0) {
+        let components = (
+            R: Double((hex >> 16) & 0xff) / 255,
+            G: Double((hex >> 08) & 0xff) / 255,
+            B: Double((hex >> 00) & 0xff) / 255
+        )
+        self.init(
+            colorSpace,
+            red: components.R,
+            green: components.G,
+            blue: components.B,
+            opacity: opacity
+        )
+    }
 
 }
