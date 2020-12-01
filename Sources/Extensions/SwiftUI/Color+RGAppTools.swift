44b668a2dc69cb1be0eb10c93bbd0af954feb88f
Smartech <rain2540@foxmail.com>
2020-12-01 17:23:50 +0800

    docs (Color) : add comment

--- Sources/Extensions/SwiftUI/Color+RGAppTools.swift
+++ Sources/Extensions/SwiftUI/Color+RGAppTools.swift
@@ -11,6 +11,11 @@ import SwiftUI
 @available(iOS 13.0, *)
 extension Color {
 
+    /// 通过指定的颜色空间、一个表示 RGB 分量值的 16 进制数字和不透明度, 返回一个颜色对象
+    /// - Parameters:
+    ///   - colorSpace: 颜色空间
+    ///   - hex: 表示 RGB 分量值的 6 位 16 进制数字
+    ///   - opacity: 不透明度 (0 ~ 1)
     init(_ colorSpace: Color.RGBColorSpace = .sRGB, hex: Int, opacity: Double = 1.0) {
         let components = (
             R: Double((hex >> 16) & 0xff) / 255,
