dc0a6136a318feafed6597f921e410c8e61039e7
Smartech <rain_2540@foxmail.com>
2021-03-16 17:18:52 +0800

    feat (UserDefaults) : remove deprecated methods

--- Sources/Extensions/Foundation/UserDefaults+RGAppTools.swift
+++ Sources/Extensions/Foundation/UserDefaults+RGAppTools.swift
@@ -23,6 +23,8 @@ public final class Key<ValueType: Codable> {
 
 extension RGAppTools where Base: UserDefaults {
 
+    // MARK: Public
+
     /// 检验 UserDefaults 中是否存在某个 key
     /// ```
     /// // How to use
@@ -98,6 +100,9 @@ extension RGAppTools where Base: UserDefaults {
         base.synchronize()
     }
 
+
+    // MARK: - Private
+
     /// 检验给定类型是否为基础类型
     /// - Parameter type: 待检验的类型
     /// - Returns: 检验结果
@@ -108,146 +113,6 @@ extension RGAppTools where Base: UserDefaults {
             default:
                 return false
         }
-    }
-
-}
-
-// MARK: - deprecated methods
-
-extension RGAppTools where Base: UserDefaults {
-
-    /// 检验 UserDefaults 中是否存在某个 key
-    /// - Parameter key: 待检验的 key
-    /// - Returns: 检验结果
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.has(_:)` instead.", renamed: "has(_:)")
-    public func isHave(key: String) -> Bool {
-        let userDefaultsDic = base.dictionaryRepresentation() as NSDictionary
-        let keys = userDefaultsDic.allKeys
-
-        for i in 0 ..< keys.count {
-            let aKey = keys[i] as? String
-            if key == aKey {
-                return true
-            }
-        }
-        return false
-    }
-
-    /// 将某个 key - Object 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - object: 将要存入的 object
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "save(_:for:)")
-    public func save(_ object: Any?, forKey key: String) {
-        base.set(object, forKey: key)
-        base.synchronize()
-    }
-
-    /// 将某个 key - Int 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - value: 将要存入的 Int 值
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "save(_:for:)")
-    public func saveInt(_ value: Int, forKey key: String) {
-        base.set(value, forKey: key)
-        base.synchronize()
-    }
-
-    /// 将某个 key - Float 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - value: 将要存入的 Float 值
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "save(_:for:)")
-    public func saveFloat(_ value: Float, forKey key: String) {
-        base.set(value, forKey: key)
-        base.synchronize()
-    }
-
-    /// 将某个 key - Double 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - value: 将要存入的 Double 值
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "save(_:for:)")
-    public func saveDouble(_ value: Double, forKey key: String) {
-        base.set(value, forKey: key)
-        base.synchronize()
-    }
-
-    /// 将某个 key - Bool 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - value: 将要存入的 Bool 值
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "save(_:for:)")
-    public func saveBool(_ value: Bool, forKey key: String) {
-        base.set(value, forKey: key)
-        base.synchronize()
-    }
-
-    /// 将某个 key - URL 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - url: 将要存入的 URL 值
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "save(_:for:)")
-    public func saveURL(_ url: URL?, forKey key: String) {
-        base.set(url, forKey: key)
-        base.synchronize()
-    }
-
-}
-
-//  MARK: Savings
-extension UserDefaults {
-
-    /// 将某个 key - Object 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - object: 将要存入的 object
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "rat.save(_:for:)")
-    public func rat_save(_ object: Any?, forKey key: String) {
-        self.set(object, forKey: key)
-        self.synchronize()
-    }
-
-    /// 将某个 key - Bool 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - value: 将要存入的 Bool 值
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "rat.save(_:for:)")
-    public func rat_saveBool(_ value: Bool, forKey key: String) {
-        self.set(value, forKey: key)
-        self.synchronize()
-    }
-
-    /// 将某个 key - Double 值存入 UserDefaults 中
-    /// - Parameters:
-    ///   - value: 将要存入的 Double 值
-    ///   - key: 将要存入的 key
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.save` instead.", renamed: "rat.save(_:for:)")
-    public func rat_saveDouble(_ value: Double, forKey key: String) {
-        self.set(value, forKey: key)
-        self.synchronize()
-    }
-
-}
-
-//  MARK: Check key exist or not
-extension UserDefaults {
-
-    /// 检验 UserDefaults 中是否存在某个 key
-    /// - Parameter key: 待检验的 key
-    /// - Returns: 检验结果
-    @available(*, deprecated, message: "Extensions directly on UserDefaults are deprecated. Use `UserDefaults.standard.rat.has(key:)` instead.", renamed: "rat.has(_:)")
-    public func rat_isHaveKey(key: String) -> Bool {
-        let userDefaultsDic = self.dictionaryRepresentation() as NSDictionary
-        let keys = userDefaultsDic.allKeys
-
-        for i in 0 ..< keys.count {
-            let aKey = keys[i] as? String
-            if key == aKey {
-                return true
-            }
-        }
-        return false
     }
 
 }
