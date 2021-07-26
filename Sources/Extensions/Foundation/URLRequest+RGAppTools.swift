55c6c54d2f1250c86cf48b51ee4d921f4bb0f838
Smartech <rain2540@foxmail.com>
2021-07-26 14:34:06 +0800

    chore (project) : add URLRequest+RGAppTools

---
+++ Sources/Extensions/Foundation/URLRequest+RGAppTools.swift
@@ -0,0 +1,13 @@
+//
+//  URLRequest+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2021/7/26.
+//  Copyright © 2021 Smartech. All rights reserved.
+//
+
+import Foundation
+
+extension URLRequest {
+
+}
