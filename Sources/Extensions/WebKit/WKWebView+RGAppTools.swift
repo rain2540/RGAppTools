c52e9821258ac1e7247a9779ee7e457991599ccd
Smartech <rain2540@foxmail.com>
2021-07-26 16:18:53 +0800

    chore (project) : add WKWebView+RGAppTools

---
+++ Sources/Extensions/WebKit/WKWebView+RGAppTools.swift
@@ -0,0 +1,16 @@
+//
+//  WKWebView+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2021/7/26.
+//  Copyright © 2021 Smartech. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+extension RGAppTools where Base: WKWebView {
+
+
+
+}
