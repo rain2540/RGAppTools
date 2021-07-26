52d01da283ab2eb9007a9b843740b4743f711f3d
Smartech <rain2540@foxmail.com>
2021-07-26 15:18:03 +0800

    chore (project) : add RATError

---
+++ Sources/RATError.swift
@@ -0,0 +1,13 @@
+//
+//  RATError.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2021/7/26.
+//  Copyright © 2021 Smartech. All rights reserved.
+//
+
+import Foundation
+
+public enum RATError: Error {
+
+}
