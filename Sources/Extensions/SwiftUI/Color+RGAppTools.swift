3f44ef520efd18c0953967e5d81e537206733ea1
Rain2540 <rain2540@foxmail.com>
2021-11-14 23:11:29 +0800

    refactor (SwiftUI.Color) : update variable name

--- Sources/Extensions/SwiftUI/Color+RGAppTools.swift
+++ Sources/Extensions/SwiftUI/Color+RGAppTools.swift
@@ -22,15 +22,15 @@ extension Color {
         opacity: Double = 1.0
     ) {
         let components = (
-            R: Double((hex >> 16) & 0xff) / 255,
-            G: Double((hex >> 08) & 0xff) / 255,
-            B: Double((hex >> 00) & 0xff) / 255
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255
         )
         self.init(
             colorSpace,
-            red: components.R,
-            green: components.G,
-            blue: components.B,
+            red: components.red,
+            green: components.green,
+            blue: components.blue,
             opacity: opacity
         )
     }
