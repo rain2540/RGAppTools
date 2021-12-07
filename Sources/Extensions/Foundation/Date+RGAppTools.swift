d3a702874b74d0ce4512a7ac0c0dbb4e723b7ca4
Smartech <rain2540@foxmail.com>
2021-12-07 14:11:30 +0800

    feat (Date) : update default value for parameter 'timeZone' of method 'dateString'

--- Sources/Extensions/Foundation/Date+RGAppTools.swift
+++ Sources/Extensions/Foundation/Date+RGAppTools.swift
@@ -100,11 +100,11 @@ extension DateExtension {
     /// 依据给定的时间格式、时区，获取某个时间对应的字符串
     /// - Parameters:
     ///   - format: 时间格式，默认为 `yyyy-MM-dd`
-    ///   - timeZone: 时区，默认为东 8 时区（北京时间）
+    ///   - timeZone: 时区，默认为系统当前使用的时区
     /// - Returns: 获取的时间字符串
     public func dateString(
         format: String = "yyyy-MM-dd",
-        timeZone: TimeZone? = TimeZone.rat.chinaMainLand
+        timeZone: TimeZone? = TimeZone.current
     ) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
