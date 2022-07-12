86a468c4a7034c8f6f98c57f00e7770ed59cf91b
Smartech <rain2540@foxmail.com>
2022-07-12 09:08:49 +0800

    style (Array) : update style

--- Sources/Extensions/Basic/Array+RGAppTools.swift
+++ Sources/Extensions/Basic/Array+RGAppTools.swift
@@ -10,56 +10,56 @@ import Foundation
 
 extension Array {
 
-    /// 获取数组特定位置的元素 / 设置数组特定元素的值
-    /// - Parameters:
-    ///   - first: 第一个特定元素的下标
-    ///   - second: 第二个特定元素的下标
-    ///   - others: 其他特定元素的下标
-    public subscript(first: Int, second: Int, others: Int...) -> ArraySlice<Element> {
-        get {
-            var indexes: [Int] = []
-            indexes.append(contentsOf: [first, second])
-            indexes.append(contentsOf: others)
+  /// 获取数组特定位置的元素 / 设置数组特定元素的值
+  /// - Parameters:
+  ///   - first: 第一个特定元素的下标
+  ///   - second: 第二个特定元素的下标
+  ///   - others: 其他特定元素的下标
+  public subscript(first: Int, second: Int, others: Int...) -> ArraySlice<Element> {
+    get {
+      var indexes: [Int] = []
+      indexes.append(contentsOf: [first, second])
+      indexes.append(contentsOf: others)
 
-            var result = ArraySlice<Element>()
-            for i in indexes {
-                assert(i < self.count, "Index is out of range")
-                result.append(self[i])
-            }
-            return result
-        }
-
-        set {
-            var indexes: [Int] = []
-            indexes.append(contentsOf: [first, second])
-            indexes.append(contentsOf: others)
-
-            for (index, i) in indexes.enumerated() {
-                assert(i < self.count, "Index is out of range")
-                self[i] = newValue[index]
-            }
-        }
+      var result = ArraySlice<Element>()
+      for i in indexes {
+        assert(i < self.count, "Index is out of range")
+        result.append(self[i])
+      }
+      return result
     }
 
-    /// 获取数组特定位置的元素 / 设置数组特定元素的值
-    /// - Parameter input: 特定元素的下标组成的数组
-    public subscript(input: [Int]) -> ArraySlice<Element> {
-        get {
-            var result = ArraySlice<Element>()
-            for i in input {
-                assert(i < self.count, "Index is out of range")
-                result.append(self[i])
-            }
-            return result
-        }
+    set {
+      var indexes: [Int] = []
+      indexes.append(contentsOf: [first, second])
+      indexes.append(contentsOf: others)
 
-        set {
-            for (index, i) in input.enumerated() {
-                assert(i < self.count, "Index is out of range")
-                self[i] = newValue[index]
-            }
-        }
+      for (index, i) in indexes.enumerated() {
+        assert(i < self.count, "Index is out of range")
+        self[i] = newValue[index]
+      }
     }
+  }
+
+  /// 获取数组特定位置的元素 / 设置数组特定元素的值
+  /// - Parameter input: 特定元素的下标组成的数组
+  public subscript(input: [Int]) -> ArraySlice<Element> {
+    get {
+      var result = ArraySlice<Element>()
+      for i in input {
+        assert(i < self.count, "Index is out of range")
+        result.append(self[i])
+      }
+      return result
+    }
+
+    set {
+      for (index, i) in input.enumerated() {
+        assert(i < self.count, "Index is out of range")
+        self[i] = newValue[index]
+      }
+    }
+  }
 
   public subscript(guarded idx: Int) -> Element? {
     guard (startIndex ..< endIndex).contains(idx) else { return nil }
