acb0e962d33a51b1cd4914da1256cd980a446cb1
Smartech <rain2540@foxmail.com>
2022-09-07 15:05:01 +0800

    chore (project) : add extension of Decimal

---
+++ Sources/Extensions/Foundation/Decimal+RGAppTools.swift
@@ -0,0 +1,13 @@
+//
+//  Decimal+RGAppTools.swift
+//  RGAppTools
+//
+//  Created by RAIN on 2022/9/7.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+extension Decimal {
+
+}
