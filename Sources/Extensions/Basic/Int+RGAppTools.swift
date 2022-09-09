07fecbcc1bcbecf68e0188e5d34de5907ceceff5
Smartech <rain2540@foxmail.com>
2022-09-09 15:24:18 +0800

    feat (Int) : add method "string(format: )"

--- Sources/Extensions/Basic/Int+RGAppTools.swift
+++ Sources/Extensions/Basic/Int+RGAppTools.swift
@@ -38,6 +38,10 @@ public struct IntExtension {
 
 extension IntExtension {
 
+  public func string(format: String) -> String {
+    return String(format: "%\(format)d", int)
+  }
+
   /// 格式化输出
   /// - Parameter fmt: 以字符串形式表示的输出格式
   /// - Returns: 格式化输出结果
