b718f1599188a4e582530e1b33b1a92c5c55b283
Smartech <rain2540@foxmail.com>
2022-11-22 16:51:49 +0800

    docs (CLLocation) : update comment

--- Sources/Extensions/CoreLocation/CLLocation+RGAppTools.swift
+++ Sources/Extensions/CoreLocation/CLLocation+RGAppTools.swift
@@ -87,7 +87,7 @@ extension RGAppTools where Base: CLLocation {
   }
 
   /// 火星坐标 ---> 百度坐标
-  ///
+  /// - GCJ-02 ---> BD-09
   /// - Parameter marsCoordinate: 记录火星坐标经纬度的结构体
   /// - Returns: 记录百度坐标经纬度的结构体
   public static func locationBaidu(fromMars marsCoordinate: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
